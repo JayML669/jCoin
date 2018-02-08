975a092c79c0a80ac977cfc545d860e794d0093e
Jay <jay@jaybirdgroup.com>
2018-02-07 18:44:03 -0700

    added proof of work transactions mining rewards and balance but broken

--- main.py
+++ main.py
@@ -1,48 +1,106 @@
 import hashlib
+import datetime
 
-sha = hashlib.sha256()
+
+
+class transaction:
+
+    def __init__(self, sender, receiver, amount):
+        self.sender = sender
+        self.receiver = receiver
+        self.amount = amount
+
+
 
 
 class block:
 
-    def __init__(self, index, timestamp, data, prevhash=''):
-        self.index = index
+    def __init__(self, timestamp, data, prevhash=''):
         self.data = data
         self.timestamp = timestamp
         self.prevhash = prevhash
+        self.mine_data = 0
         self.hash = self.get_hash()
 
+
+
     def get_hash(self):
+        sha = hashlib.sha256()
         if type(self.data) is not str:
-            data_string = ''.join('{}{}'.format(key, val) for key, val in self.data.items())
-            sha.update(str(str(self.index)+data_string+self.timestamp+self.prevhash).encode('utf-8'))
+            data_string = ''.join('[{}{}{}] '.format(send, receive, amt) for trans.sender, trans.receiver, trans.amount in trans for trans in data)
+            sha.update(str(str(self.mine_data)+data_string+self.timestamp+self.prevhash).encode('utf-8'))
         else:
-            sha.update(str(str(self.index)+self.data+self.timestamp+self.prevhash).encode('utf-8'))
+            sha.update(str(str(self.mine_data)+self.data+self.timestamp+self.prevhash).encode('utf-8'))
 
         return sha.hexdigest()
 
     def print_block(self):
-        print(str(str(self.index)+' '+self.data+' '+self.timestamp+' '+self.prevhash+' '+self.hash))
+        print(str(self.data+' '+self.timestamp+' '+self.prevhash+' '+self.hash))
+
+    def mine_block(self, difficulty):
+        while self.hash[0:difficulty] != '0'*difficulty:
+            self.mine_data += 1
+            self.hash = self.get_hash()
+
+        print("Block mined: " + self.hash)
 
 
 class chain:
 
     def __init__(self):
         self.chain = [self.create_genesis_block()]
+        self.difficulty = 4
+        self.pending_transactions = []
+        self.mining_reward = 8
 
     def create_genesis_block(self):
-        return block(0, '01/01/2018', 'Genesis Block', '')
+        return block('01/01/2018', 'Genesis Block', '')
 
     def get_latest_block(self):
         return self.chain[len(self.chain) - 1]
 
-    def add_block(self, new_block):
-        new_block.prevhash = self.get_latest_block().hash
-        new_block.hash = new_block.get_hash()
+    def mine_pending_transctions(self, miner_adress):
+        self.pending_transactions.append(transaction(None, miner_adress, self.mining_reward))
+        new_block = block(datetime.datetime.now().strftime('%d/%m/%y %M:%H:%S'), self.pending_transactions)
+        new_block.mine_block(self.difficulty)
+
+        print('Block successfully mined...')
         self.chain.append(new_block)
+        self.pending_transactions = []
+
+    def add_transaction(self, transaction):
+        self.pending_transactions.append(transaction)
+
+    def get_balance(self, adress):
+        balance = 0
+        for block in self.chain:
+            for transaction in block:
+                if transaction.sender == adress:
+                    balance -= transaction.amount
+                elif transaction.receiver == adress:
+                    balance += transaction.amount
+        return balance
+
+    def validate(self):
+        for i in range(1,len(self.chain)-1):
+            block_check = self.chain[i]
+            previous_block_check = self.chain[i-1]
+            if block_check.hash != block_check.get_hash():
+                return False
+            elif block_check.prevhash != previous_block_check.hash:
+                return False
+            else:
+                return True
+
+
+
 
 
 jCoin = chain()
 
-jCoin.add_block(block(1, '01/31/2018', {'amount': 4}))
-jCoin.add_block(block(2, '02/01/2018', {'amount': 4}))
+jCoin.add_transaction(transaction('adress1','adress2', 16))
+print('\n Strarting the miner...')
+jCoin.mine_pending_transctions('test-adress')
+
+print('test-adress balance is '+jCoin.get_balance('test-adress'))
+print('Is chain valid?' + str(jCoin.validate()))
