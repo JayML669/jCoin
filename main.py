268007246d1026b7c603493d784a2df9a4c58527
Jay <jay@jaybirdgroup.com>
2018-02-14 18:39:20 -0700

    Started client blockchain seperation process

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import datetime
 import pickle
 import csv
 import os
+import time
 
 class transaction:
 
@@ -126,3 +127,36 @@ else:
     jCoin = chain()
     with open('block_chain.pickle', 'wb') as f:
         pickle.dump(jCoin, f)
+
+if not os.path.exists('pending_transactions.csv'):
+    f = open('pending_transactions.csv', 'w')
+    f.close()
+
+if not os.path.exists('/pending_blocks'):
+    os.makedirs('/pending_blocks')
+
+
+
+
+if __name__ == '__main__':
+    while True:
+        with open('block_chain.pickle', 'wb') as f:
+            pickle.dump(jCoin, f)
+
+        if os.stat('pending_transactions.csv').st_size !=0:
+            with open('pending_transactions.csv') as file:
+                reader = csv.reader(file)
+                for row in reader:
+                    add_transaction(transaction(row[0],row[1],row[2]))
+
+                file.turnicate()
+
+        for filename in os.listdir('/pending_blocks'):
+            if filename.endswith('.pickle'):
+                with open('block_chain.pickle', 'rb') as f:
+                    test_chain = pickle.load(f)
+                with open(filename, 'r') as f:
+                    test_block = pickle.load(f)
+                test_chain.chain.append(test_block)
+                if test_chain.validate() == True:
+                    jCoin.append(test_block)
